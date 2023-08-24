9472509fcc6443d2dffb1c24ef9da3a24699ed2b
Kevin Aguirre <kevinaguirre463@gmail.com>
2023-08-24 12:30:03 -0400

    Added comments to App.js

--- js/App.js
+++ js/App.js
@@ -1,19 +1,26 @@
 import NotesView from "./NotesView.js";
 import NotesAPI from "./notesapi.js";
 
+// defines and export App 
 export default class App {
     constructor(root) {
-        this.notes = [];
+        // intialize notes state
+        this.notes = []; 
         this.activeNote = null;
+
+        // creates notes view isntance and provides event handlers
         this.view = new NotesView(root, this._handlers());
 
+        // loads and displays notes from local storage
         this._refreshNotes();
     }
 
+    // loads notes from local storage and updates UI 
     _refreshNotes() {
         const notes = NotesAPI.getAllNotes();
         this._setNotes(notes);
 
+        // sets first nots as active if notes list is not empty
         if (notes.length > 0) {
             this._setActiveNote(notes[0])
         };
@@ -21,12 +28,14 @@ export default class App {
 
     }
 
+    // update notes & refresh ui 
     _setNotes(notes) {
         this.notes = notes;
         this.view.updateNoteList(notes);
 
     }
 
+    // sets a note as active
     _setActiveNote(note) { 
         this.activeNote = note;
         this.view.updateActiveNote(note);
@@ -35,23 +44,29 @@ export default class App {
 
     }
 
+    // defines event handlers
     _handlers() {
         return {
+            // handles note selection
             onNoteSelect: noteId => {
                 const selectedNote = this.notes.find(note => note.id == noteId);
                 this._setActiveNote(selectedNote);
 
             },
+            // handles adding a new note
             onNoteAdd: () => {
                 const newNote = {
                     title:"New Note",
                     body: "Take note..."
                 };
 
+                // saves the note & refreshes the notes list
                 NotesAPI.saveNote(newNote);
                 this._refreshNotes();
 
             },
+
+            // handles editing a note
             onNoteEdit: (title, body) => {
                 NotesAPI.saveNote({
                     id: this.activeNote.id,
@@ -62,6 +77,8 @@ export default class App {
 
                 this._refreshNotes();
             },
+
+            // handles note deletion
             onNoteDelete: noteId => {
                 NotesAPI.deleteNote(noteId);
                 this._refreshNotes();
