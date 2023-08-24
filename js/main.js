3be8987879168fe00b00eb9bb12e1a8470d49891
Kevin Aguirre <kevinaguirre463@gmail.com>
2023-08-23 22:50:01 -0400

    Added TODO tasks in main.js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ import App from "./App.js"
 
 const root = document.getElementById("app");
 const app = new App(root);
+
+//TODO: Attempt to add a codeblocks feature: click a button to intialize a code block, and then specify programming langauge.
+//TODO: Attempt to add a bullet point feature; click on a button to make a bullet point, make another one using enter, supports indentations.
+//TODO: Add a bullet point list feature
