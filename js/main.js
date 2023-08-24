a7759116f70eafd5267ecce821045696759dba27
Kevin Aguirre <kevinaguirre463@gmail.com>
2023-08-24 12:32:45 -0400

    Moved to-do tasks to to-do.txt

--- js/main.js
+++ js/main.js
@@ -2,15 +2,3 @@ import App from "./App.js"
 
 const root = document.getElementById("app");
 const app = new App(root);
-
-
-/*
-TODO: Attempt to add a codeblocks feature: 
-TODO: Attempt to add a bullet point feature;
-TODO: Add color themes feature + Dark Mode
-TODO: Rich Text Formatting: Make text bold, italics, underlined.
-TODO: Add Tags or Categories: Dividers between notes and label them, directory system?
-TODO: Add Voice Notes
-TODO: Search notes feature
-
-*/
