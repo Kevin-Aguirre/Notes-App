e51d10378b8f9b1fa62ca9ac6dceca753419c4fc
Kevin Aguirre <kevinaguirre463@gmail.com>
2023-08-23 22:51:50 -0400

    updated TODO tasks in main.js; added theme task

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ import App from "./App.js"
 const root = document.getElementById("app");
 const app = new App(root);
 
-//TODO: Attempt to add a codeblocks feature: click a button to intialize a code block, and then specify programming langauge.
-//TODO: Attempt to add a bullet point feature; click on a button to make a bullet point, make another one using enter, supports indentations.
-//TODO: Add a bullet point list feature
+
+/*
+TODO: Attempt to add a codeblocks feature: click a button to intialize a code block, and then specify programming langauge.
+TODO: Attempt to add a bullet point feature; click on a button to make a bullet point, make another one using enter, supports indentations.
+TODO: Add a bullet point list feature
+TODO: Add a Theme Feature, this might be good in the header.
+*/
